1a74b76412bd673067b58a4ca593e57c4d59a359
José Mota <jose.a.mota11@gmail.com>
2023-08-11 20:47:33 -0400

    :sparkles: Prevent server from adding multiple posts with the same id

--- src/index.js
+++ src/index.js
@@ -2,11 +2,29 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { routes } from './routes/index.js';
 import { getPosts } from './handlers/index.js';
+import data from '../data.json' assert { type: 'json' };
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+// Middleware
+app.use(express.json());
+app.post(routes.post, (req, res, next) => {
+  const { id } = req.body;
+  const idToInt = parseInt(id);
+  const foundPost = data.some(({ id }) => id === idToInt);
+
+  if (foundPost) {
+    res
+      .status(403)
+      .json({ message: 'This is is already used, please use another' });
+    return;
+  }
+
+  next();
+});
 
 app.get(routes.posts, getPosts);
 
@@ -15,15 +33,15 @@ app.get(routes.postById, (req, res) => {
 });
 
 app.post(routes.post, (req, res) => {
-  res.json({ massage: 'Hello World' });
+  res.json({ massage: 'Creating a post' });
 });
 
 app.put(routes.postById, (req, res) => {
-  res.json({ massage: 'Hello World' });
+  res.json({ massage: 'Updating a post' });
 });
 
 app.delete(routes.postById, (req, res) => {
-  res.json({ massage: 'Hello World' });
+  res.json({ massage: 'Deleting a post' });
 });
 
 app.listen(PORT, () => {
